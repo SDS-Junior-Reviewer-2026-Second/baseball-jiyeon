2bec1f8d4620744598f782cb8aca34b78504871d
jiyeonyooo <ji04230423@gmail.com>
2026-09-18 13:08:09 +0900

    feat: input으로 문자열도 가능하도록 로직 추가

--- src/main/java/org/example/baseball/Game.java
+++ src/main/java/org/example/baseball/Game.java
@@ -10,6 +10,12 @@ public class Game {
             throw new IllegalArgumentException();
         }
 
+        for (char number : guessNumber.toCharArray()) {
+            if(number < '0' || number > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+
     }
 }
 
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -34,6 +34,7 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 
 
