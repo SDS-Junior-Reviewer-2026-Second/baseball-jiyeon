0f5d9942d7d92b81fb559aedd4a6a6b61c7065bd
jiyeonyooo <ji04230423@gmail.com>
2026-09-18 13:37:01 +0900

    refactor: 메소드 및 변수 정의에 따른 추상화 (refactoring)

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -41,27 +41,28 @@ public class GameTest {
         assertIllegalArgument("121");
     }
 
+    private void generateQuestion(String questionNumber) {
+        game.question = questionNumber;
+    }
+
 
     @Test
-    void returnSolvedREsultIfMatchedNumber() {
-        game.question = "123";
-        GuessResult result = game.guess("123");
-
-        assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(true);
-        assertThat(result.getStrikes()).isEqualTo(3);
-        assertThat(result.getBalls()).isEqualTo(0);
+    void returnSolvedResultIfMatchedNumber() {
+        generateQuestion("123");
+        assertMatchedNumber(game.guess("123"), true, 3, 0);
     }
 
     @Test
-    void returnSolvedREsultIfUnMatchedNumber() {
-        game.question = "123";
-        GuessResult result = game.guess("456");
+    void returnSolvedResultIfUnMatchedNumber() {
+        generateQuestion("123");
+        assertMatchedNumber(game.guess("456"), false, 0, 0);
+    }
 
+    private static void assertMatchedNumber(GuessResult result, boolean solved, int strikes, int balls) {
         assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(false);
-        assertThat(result.getStrikes()).isEqualTo(0);
-        assertThat(result.getBalls()).isEqualTo(0);
+        assertThat(result.isSolved()).isEqualTo(solved);
+        assertThat(result.getStrikes()).isEqualTo(strikes);
+        assertThat(result.getBalls()).isEqualTo(balls);
     }
 
 
